a354847f0ec0c2e8acd5ce545dc1bfc44b4d5d85
kckimc89 <kckimc89@daum.net>
2026-09-23 11:49:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-const C='car-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='car-v13';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'])))});
+self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==C)await caches.delete(k);await self.clients.claim()})()));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));return;}
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const x=resp.clone();caches.open(C).then(c=>c.put(e.request,x));return resp})));
+});
